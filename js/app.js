7c85e7484732aab424075400694074268db936ee
Lucas Vale <lucasvale0904@gmail.com>
2024-10-27 17:41:40 -0300

    V1.0 comentado

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 function alterarStatus(id){
+    //Pegando informações osbre o botão 
     let gameClicado = document.getElementById(`game-${id}`);
     let imagem = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
     let nome = gameClicado.querySelector('.dashboard__item__name');
+    //Alugando ou devolvendo o jogo
     if (imagem.classList == 'dashboard__item__img dashboard__item__img--rented'){
+        //Confirmação se deseja alugar 
         var resultado = confirm("Deseja devolver o jogo: " + (nome.textContent) + " ?");
         if (resultado == true) {
             imagem.classList = 'dashboard__item__img' ;
@@ -17,6 +20,7 @@ function alterarStatus(id){
             alert("Você desistiu de devolver o jogo " + itemSelecionado);
         }
     }else{
+        //Confirmação se deseja devolver 
         var resultado = confirm("Deseja alugar o jogo: " + (nome.textContent) + " ?");
         if (resultado == true) {
             imagem.classList = 'dashboard__item__img dashboard__item__img--rented' ;
@@ -31,10 +35,5 @@ function alterarStatus(id){
             alert("Você desistiu de alugar o jogo " + (nome.textContent) + " do nosso catalogo!");
             
         }
-        imagem.classList = 'dashboard__item__img dashboard__item__img--rented' ;
-
-        botao.classList = 'dashboard__item__button dashboard__item__button--return' ;
-        
-        botao.textContent = 'Devolver' ;
     }
 }
